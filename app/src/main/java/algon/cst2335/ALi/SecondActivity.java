49019ca2ef76dd892ba68ff0cbff3df1b7de7d49
Ali Komijani <komi0003@algonquinlive.com>
2023-11-01 10:19:13 -0400

    Week 4, finish

--- app/src/main/java/algon/cst2335/ALi/SecondActivity.java
+++ app/src/main/java/algon/cst2335/ALi/SecondActivity.java
@@ -1,12 +1,21 @@
 package algon.cst2335.ALi;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -15,17 +24,40 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button PicChange = findViewById(R.id.button3);
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("EmailAddress");
         TextView textview = findViewById(R.id.textView);
         textview.setText("Welcome back " + emailAddress);
         EditText ed = findViewById(R.id.editTextPhone);
         Button callbotton = findViewById(R.id.button2);
-        callbotton.setOnClickListener( clk-> {
+        callbotton.setOnClickListener(clk -> {
             String phoneNumber = ed.getText().toString();
             Intent call = new Intent(Intent.ACTION_DIAL);
             call.setData(Uri.parse("tel:" + phoneNumber));
             startActivity(call);
-        } );
+        });
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        ImageView profileImage = findViewById(R.id.imageView);
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Bitmap thumbnail = data.getParcelableExtra("data");
+                            profileImage.setImageBitmap(thumbnail);
+                        }
+                    }
+                });
+
+        PicChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraResult.launch(cameraIntent);
+            }
+        });
     }
 }
